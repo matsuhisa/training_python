25822bc93bef317b3a1b46d64f239b851840b9ce
matsuhisa <matsuhisa+github@gmail.com>
2018-05-29 09:37:36 +0900

    Update Dog :dog:

--- dog.py
+++ dog.py
@@ -1,5 +1,8 @@
 class Dog:
-  kind = 'canine'
+    tricks = []
 
-  def __init__(self, name):
-    self.name = name
+    def __init__(self, name):
+        self.name = name
+
+    def add_trick(self, trick):
+        self.tricks.append(trick)
