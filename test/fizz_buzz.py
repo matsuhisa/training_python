65126fdbebb9d039ad4ffd77e36ae688764472d5
matsuhisa <matsuhisa+github@gmail.com>
2018-06-06 09:47:31 +0900

    Update :snake:

--- test/fizz_buzz.py
+++ test/fizz_buzz.py
@@ -1,12 +1,17 @@
 class FizzBuzz:
 
     def run(self, number):
-        print(number);
-
+        if number > 0:
+            if (number % 5) == 0 and (number % 3) == 0:
+                return "FizzBuzz"
+            if (number % 5) == 0:
+                return "Buzz"
+            if (number % 3) == 0:
+                return "Fizz"
+        return number
 
 # f = FizzBuzz()
-#
-# f.run(1)
-# f.run(3)
-# f.run(5)
-# f.run(15)
+# print(f.run(1))
+# print(f.run(3))
+# print(f.run(5))
+# print(f.run(15))
--- test/fizz_buzz_test.py
+++ test/fizz_buzz_test.py
@@ -3,3 +3,19 @@ from fizz_buzz import FizzBuzz
 def test_run():
     f = FizzBuzz()
     assert f.run(1) == 1
+
+def test_run3():
+    f = FizzBuzz()
+    assert f.run(3) == "Fizz"
+
+def test_run5():
+    f = FizzBuzz()
+    assert f.run(5) == "Buzz"
+
+def test_run15():
+    f = FizzBuzz()
+    assert f.run(15) == "FizzBuzz"
+
+def test_run0():
+    f = FizzBuzz()
+    assert f.run(0) == 0
